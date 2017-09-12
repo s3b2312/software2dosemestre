7774c28d1e6843e97b416d42b6eb2da84e6ee21a
vitokooo <vgongora.92@gmail.com>
2017-09-12 17:50:48 -0300

    Se traduce las vistas de crear usuario generados por la gema clearance de ingles a español

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,27 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  # Escogue que url ocupara para el mailer segun el ambiente
+  if Rails.env.development? || Rails.env.test?
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  elsif Rails.env.production?
+    config.action_mailer.default_url_options = {:host => ENV['HOST']}
+  end
+
+  # Configuracion para smtp con cuenta gmail
+   config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "mail.proyecto.soft@gmail.com",
+    :password => "mailsoft123",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+    }
+
 end
